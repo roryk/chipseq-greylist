a8dd597c532eef662af50a08f41f9eb294e6c0cc
roryk <rory.kirchner@gmail.com>
2017-11-08 10:56:30 -0500

    lose .py from chipseq-greylist script

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
+from setuptools import setup
 version = "1.0.0"
 
 install_requires = ["pandas", "numpy", "scipy", "statsmodels"] 
 
-zip_safe = False
-scripts = ['scripts/chipseq-greylist.py']
+scripts = ['scripts/chipseq-greylist']
 
-write_version_py()
 setup(name="chipseq-greylist",
       version=version,
       author="Rory Kirchner",
       author_email="roryk@alum.mit.edu",
-      description="Best-practice pipelines for fully automated high throughput sequencing analysis",
+      description="python implementation of GreyListChIP",
       license="MIT",
       url="https://github.com/roryk/chipesq-greylist",
-      zip_safe=zip_safe,
       scripts=scripts,
-      install_requires=install_requires,
-      include_package_data=True)
+      install_requires=install_requires)
